7d9a4c4add84c91efc463dec31825a45cd70a20e
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2020-05-15 14:33:42 +0200

    Added coloring to source and sink nodes

--- src/GUI/OriginalGraphDrawer.java
+++ src/GUI/OriginalGraphDrawer.java
@@ -4,20 +4,15 @@ import Graph.*;
 import edu.uci.ics.jung.algorithms.layout.CircleLayout;
 import edu.uci.ics.jung.algorithms.layout.Layout;
 import edu.uci.ics.jung.graph.AbstractGraph;
-import edu.uci.ics.jung.graph.DirectedSparseMultigraph;
 import edu.uci.ics.jung.graph.SparseMultigraph;
 import edu.uci.ics.jung.graph.util.EdgeType;
 import edu.uci.ics.jung.visualization.BasicVisualizationServer;
 import edu.uci.ics.jung.visualization.decorators.ToStringLabeller;
 import edu.uci.ics.jung.visualization.renderers.Renderer;
-import edu.uci.ics.jung.visualization.*;
-import javafx.scene.shape.Circle;
 import org.apache.commons.collections15.Transformer;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.geom.Ellipse2D;
 import java.util.ArrayList;
 
@@ -25,11 +20,18 @@ public class OriginalGraphDrawer
 {
     AbstractGraph G;
 
-    public OriginalGraphDrawer(int nV, ArrayList<DirectedEdge> edgeList)
+    public OriginalGraphDrawer(int nV, ArrayList<DirectedEdge> edgeList, Vertex src, Vertex snk)
     {
         G = new SparseMultigraph<Vertex, DirectedEdge>();
         for (int i = 0; i < nV; ++i)
-            G.addVertex(new Vertex(i));
+        {
+            Vertex u = new Vertex(i);
+            if (i == src.getIndex())
+                u.setSource(true);
+            if (i == snk.getIndex())
+                u.setSink(true);
+            G.addVertex(u);
+        }
 
         for (DirectedEdge e : edgeList)
             addEdge(e);
@@ -66,6 +68,50 @@ public class OriginalGraphDrawer
 
     private void setDrawingStyle(BasicVisualizationServer<Vertex, DirectedEdge> vs)
     {
+        // vertices
+        vs.getRenderContext().setVertexLabelTransformer(new ToStringLabeller());
+        Transformer<Vertex, Font> vertexFontTransformer = new Transformer<Vertex, Font>()
+        {
+            @Override
+            public Font transform(Vertex vertex)
+            {
+                return new Font(vertex.toString(), 0, 20);
+            }
+        };
+        Transformer<Vertex, Shape> vertexShapeTransformer = new Transformer<Vertex, Shape>()
+        {
+            @Override
+            public Shape transform(Vertex vertex)
+            {
+                return new Ellipse2D.Double(-15, -15, 50, 50);
+            }
+        };
+        Transformer<Vertex,Paint> vertexColor = new Transformer<Vertex,Paint>() {
+            @Override
+            public Paint transform(Vertex u) {
+                if (u.isSource())
+                    return Color.GREEN;
+                if (u.isSink())
+                    return Color.YELLOW;
+                return Color.RED;
+            }
+        };
+        vs.getRenderContext().setVertexFontTransformer(vertexFontTransformer);
+        vs.getRenderContext().setVertexShapeTransformer(vertexShapeTransformer);
+        vs.getRenderContext().setVertexFillPaintTransformer(vertexColor);
+        vs.getRenderer().getVertexLabelRenderer().setPosition(Renderer.VertexLabel.Position.CNTR);
 
+
+        // edges
+        Transformer<DirectedEdge, Font> edgeFontTransformer = new Transformer<DirectedEdge, Font>()
+        {
+            @Override
+            public Font transform(DirectedEdge edge)
+            {
+                return new Font(edge.toString(), 0, 20);
+            }
+        };
+        vs.getRenderContext().setEdgeLabelTransformer(new ToStringLabeller<>());
+        vs.getRenderContext().setEdgeFontTransformer(edgeFontTransformer);
     }
 }
