074296994969a419c384208532b235c4c6f525bb
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2020-05-15 13:08:43 +0200

    Added support for input edges

--- src/GUI/Input.java
+++ src/GUI/Input.java
@@ -1,26 +1,30 @@
 package GUI;
 
-import Graph.DirectedEdge;
-import Graph.Vertex;
-
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class Input extends JPanel
 {
 
-    private ArrayList<DirectedEdge> edgeList;
-    Vertex src, snk;
+    private ArrayList<JTextField> edgeListTF; // TF for TextField
+    private JTextField srcTF, snkTF;
+    private JPanel edgesPanel; // Edges input From, To, Cap
+    JComboBox nVertices;
+    JComboBox nEdges;
     public Input()
     {
-        this.edgeList = new ArrayList<>();
+        this.edgeListTF = new ArrayList<>();
+        srcTF = new JTextField();
+        snkTF = new JTextField();
         createSetupPanel();
     }
 
     private JPanel createVEInputPanel()
     {
-        JPanel form = new JPanel(new GridLayout(3, 3));
+        JPanel form = new JPanel(new GridLayout(5, 5));
 
         int MAX_NODES = 100;
         Integer[] numbers = new Integer[MAX_NODES];
@@ -28,22 +32,74 @@ public class Input extends JPanel
             numbers[i] = i + 1;
 
         JLabel nVerticesLabel = new JLabel("Select number of vertices");
-        JComboBox nVertices = new JComboBox<>(numbers);
+        nVertices = new JComboBox<>(numbers);
         JLabel nEdgesLabel = new JLabel("Select number of edges");
-        JComboBox nEdges = new JComboBox<>(numbers);
+        nEdges = new JComboBox<>(numbers);
+        JLabel srcLabel = new JLabel("Enter source node: ");
+        JLabel snkLabel = new JLabel("Enter destination node: ");
 
         form.add(nVerticesLabel);
         form.add(nVertices);
         form.add(nEdgesLabel);
         form.add(nEdges);
+        form.add(srcLabel);
+        form.add(srcTF);
+        form.add(snkLabel);
+        form.add(snkTF);
+
+
+        // Action Listeners
+        nEdges.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                reinitializeEdgesPanel();
+            }
+        });
+
         return form;
+    }
+
+    private void reinitializeEdgesPanel()
+    {
+        edgesPanel.removeAll();
+        edgesPanel.add(createEdgesInput((Integer) nEdges.getSelectedItem()));
+        edgesPanel.revalidate();
+        edgesPanel.repaint();
+    }
+
+    private JPanel createEdgesInput(Integer nEdges)
+    {
+        edgeListTF.clear();
+        JPanel panel = new JPanel(new GridLayout(nEdges + 1, 2));
+        panel.add(new JLabel("From"));
+        panel.add(new JLabel("To"));
+        panel.add(new JLabel("Capacity"));
+        Dimension size = new Dimension(50, 20);
+        for (int i = 0; i < nEdges; ++i)
+        {
+            JTextField u = new JTextField();
+            JTextField v = new JTextField();
+            JTextField cost = new JTextField();
+            u.setPreferredSize(size);
+            v.setPreferredSize(size);
+            cost.setPreferredSize(size);
+            panel.add(u);
+            panel.add(v);
+            panel.add(cost);
+            edgeListTF.add(u);
+            edgeListTF.add(v);
+            edgeListTF.add(cost);
+        }
+        return panel;
     }
 
     private JScrollPane createEdgesInputPanel()
     {
-        JPanel edges = new JPanel(false);
-        edges.setAutoscrolls(true);
-        JScrollPane edgesMain = new JScrollPane(edges);
+        edgesPanel = new JPanel(false);
+        edgesPanel.setAutoscrolls(true);
+        JScrollPane edgesMain = new JScrollPane(edgesPanel);
         edgesMain.setPreferredSize(new Dimension(600, 450));
         return edgesMain;
     }
